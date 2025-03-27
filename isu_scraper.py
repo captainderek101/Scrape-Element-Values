39a8295ee1da4da0afecd95656a731e1aaf7379a
Derek Reynolds <superDreynolds@gmail.com>
2025-03-27 18:55:44 -0500

    Update isu_scraper.py

--- isu_scraper.py
+++ isu_scraper.py
@@ -9,8 +9,7 @@ root_url = 'https://coursefinder.illinoisstate.edu'
 class_to_find = 'section_mid'
 
 # function to extract html document from given url 
-def getHTMLdocument(url): 
-      
+def getHTMLdocument(url):
     # request for HTML document of given url 
     response = requests.get(url) 
       
@@ -19,7 +18,6 @@ def getHTMLdocument(url):
 
 # create document 
 html_document = getHTMLdocument(search_url) 
-
 # create soap object 
 soup = BeautifulSoup(html_document, 'html.parser') 
 
@@ -27,25 +25,19 @@ li_elements = soup.find_all('li')
 
 a_elements = []
 for li in li_elements:
-    [a_elements.append(x) for x in li.findChildren('a', recursive=False)]
+    [a_elements.append(x) for x in li.find_all('a', recursive=False)]
 
 urls_to_scrape = []
 [urls_to_scrape.append(x['href']) for x in a_elements]
 
-element_values = []
-
 for url_to_scrape in urls_to_scrape:
     # create document 
     html_document = getHTMLdocument(root_url+url_to_scrape) 
-    
     # create soap object 
     soup = BeautifulSoup(html_document, 'html.parser') 
 
-    class_elements = soup.find(class_=class_to_find).findChildren("p", recursive=False)
+    class_elements = soup.find(class_=class_to_find).find_all('p', recursive=False)
 
     # find all values of those elements
-    [element_values.append(x.get_text()) for x in class_elements] # if x.get_text() == re.compile(".*Description.*")
-
-# show all values
-for element in element_values:
-    print(element)
+    print(url_to_scrape)
+    [print(x.get_text()) for x in class_elements if re.match(re.compile('.*Description:.*'), x.get_text())]
